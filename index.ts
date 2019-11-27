40f7bf890bdd913399a9e94ae1333af584a4f228
Inaki Anduaga <inaki.anduaga@scout24.com>
2019-11-27 12:43:32 +0100

    Include extractors into main API
    Allows for easy customization of activationContext

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import _client, { ToguruClient } from './src/client'
 import * as _expressBridge from './src/express/bridge'
+import * as extractors from './src/express/extractors'
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -15,4 +16,16 @@ export const toguruClient = _client
 export const toguruExpressBridge = {
     middleware: _expressBridge.middleware,
     client: _expressBridge.expressClient,
+    /**
+     * Helper methods to extract information from the `Request`
+     */
+    requestExtractors: {
+        cookieValue: extractors.cookieValue,
+        forcedToggles: {
+            fromCookie: extractors.fromCookie,
+            fromHeader: extractors.fromHeader,
+            fromQueryParam: extractors.fromQueryParam,
+            default: extractors.defaultForcedTogglesExtractor,
+        },
+    },
 }
