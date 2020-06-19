544ae097d26899d21170a3d0d950a522a9a6a905
Enrico <enrico.vanghetti@autoscout24.com>
2020-06-19 03:04:37 +0200

    Improve tests for isToggleEnabled

--- src/services/calculateBuckets.ts
+++ src/services/calculateBuckets.ts
@@ -1,11 +1,7 @@
 import BN from 'bn.js'
 
 export default (uuid: string, defaultValue = 100): number => {
-    // This calculation is broken.
-    // Normally we would calculate with new BN(x).mod(new BN(100)).toNumber() + 1;
-    // BUT: the existing scala client is doing strange calculation.
-    // See thread here: https://github.com/AutoScout24/toguru-scala-client/pull/27
-
+    // Bucket calculation aligned with https://github.com/AutoScout24/toguru-scala-client/pull/27
     const strippedUUID = uuid.replace(/-/g, '')
     const isUUIDInvalid = strippedUUID.length !== 32
 
@@ -16,10 +12,12 @@ export default (uuid: string, defaultValue = 100): number => {
     const hi = new BN(strippedUUID.substr(0, 16), 16)
     const lo = new BN(strippedUUID.substr(16, 16), 16)
 
-    return lo
+    return (
+        lo
             .ishln(64)
             .iadd(hi)
             .fromTwos(128)
             .umod(new BN(100))
             .toNumber() + 1
+    )
 }
