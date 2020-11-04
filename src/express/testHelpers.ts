e9b48d43a61a0e8c81ca478eea850a51dce25caa
Enrico <enrico.vanghetti@autoscout24.com>
2020-11-04 16:05:55 +0100

    Fix issues on test helpers

--- src/express/testHelpers.ts
+++ src/express/testHelpers.ts
@@ -4,12 +4,12 @@ import { Toggle } from '../models/Toggle'
 import { Toggles } from '../models/Toggles'
 import { TogglingApi } from '../client'
 
+export const stubClient = (overrides: ToggleState[]) => (_: Request): TogglingApi => ({
+    isToggleEnabled: (toggle: Toggle) => overrides.find((t) => t.id == toggle.id)?.enabled ?? toggle.default,
+    togglesForService: (_: string) => new Toggles([]),
+})
+
 export const stubMiddleware = (overrides: ToggleState[]) => (req: Request, _: Response, next: NextFunction) => {
     req.toguru = stubClient(overrides)(req)
     next()
 }
-
-export const stubClient = (overrides: ToggleState[]) => (_: Request): TogglingApi => ({
-    isToggleEnabled: (toggle: Toggle) => overrides.find((t) => t.id == toggle.id)?.enabled ?? toggle.default,
-    togglesForService: (_: String) => new Toggles([]),
-})
